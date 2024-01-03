9512d1e732b27fb689bd52ad1534ff4855d6ffc5
GreySeeletsa <seeletsagrey@gmail.com>
2024-01-03 11:42:08 +0000

    handle requirement fail

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,8 +1,7 @@
-import uploadPhoto from "./utils.js";
-import createUser from "./utils.js";
+import { createUser, uploadPhoto } from './utils';
 
 export default function handleProfileSignup() {
-	return Promise.all([uploadPhoto, createUser])
+	return Promise.all([uploadPhoto(), createUser()])
 	.then((userResp) => {
 		console.log(`${userResp[0].body} ${userResp[1].firstName} ${userResp[1].lastName}`);
 	})
