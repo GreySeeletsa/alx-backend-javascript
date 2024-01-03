ef463927a234a9d29e2ee56e38cef69e7aebe612
GreySeeletsa <seeletsagrey@gmail.com>
2024-01-03 11:35:44 +0000

    handle requirement fail

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,12 +1,12 @@
-import uploadPhoto from "./utils.js"
-import createUser from "./utils.js"
+import uploadPhoto from "./utils.js";
+import createUser from "./utils.js";
 
 export default function handleProfileSignup() {
-	Promise.all([uploadPhoto, createUser])
+	return Promise.all([uploadPhoto, createUser])
 	.then((userResp) => {
 		console.log(`${userResp[0].body} ${userResp[1].firstName} ${userResp[1].lastName}`);
 	})
-	.catch((error) => {
+	.catch(() => {
 		console.log('Signup system offline');
 	});
 }
