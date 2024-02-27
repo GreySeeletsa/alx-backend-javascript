49e38ba1670e3574b754099ebd533fb660eb0c77
GreySeeletsa <seeletsagrey@gmail.com>
2024-02-27 13:55:31 +0000

    fixing requirement errors

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,9 +1,10 @@
-/** function named displayMessage that prints in STDOUT the string argument
- *  @param {str} string - The message to be printed.
+/**
+ * function named displayMessage that prints in STDOUT the string argument
+ * @param {str} string - The message to be printed.
  */
 
-function displayMessage(str) {
-	console.log(str);
+function displayMessage (str) {
+  console.log(str);
 }
 
 module.exports = displayMessage;
