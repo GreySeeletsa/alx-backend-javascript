6669dd1b1fe0eba9f0782e291c476b80fafc89c8
GreySeeletsa <seeletsagrey@gmail.com>
2024-02-29 14:40:00 +0000

    Created 6-payment_token.js and 6-payment_token.test.js

---
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -0,0 +1,17 @@
+/**
+ * Simulates an API call to get a payment token
+ */
+function getPaymentTokenFromAPI(success) {
+  // eslint-disable-next-line no-unused-vars
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ data: 'Successful response from the API' });
+    } else {
+      // We are not doing anything if success is false, 
+      // but we can reject the promise if desired
+      // reject('Error: API call failed');
+    }
+  });
+}
+
+module.exports = getPaymentTokenFromAPI;
